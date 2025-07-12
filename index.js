696fd0d1c07c440469ff9bcc2cb44abaf9724d0f
abdullah0x5c <1abdullahraja1@gmail.com>
2025-07-12 16:34:07 +0500

    deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 
 morgan.token('postData', function(req, res) {return (req.method==="POST")?(JSON.stringify(req.body)):("")} )
